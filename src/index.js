bc7cbb275609b240afbfbd9abe58bfd0e732a57e
locnx-iotvn <=>
2020-12-11 17:24:48 +0700

    MVC

--- src/index.js
+++ src/index.js
@@ -4,26 +4,32 @@ var morgan = require('morgan');
 const handlebars  = require('express-handlebars');
 const app = express();
 const port = 3100;
+const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('public'));
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
 // HTTP log
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 // Template engine
+
+// MVC: Action -> Dispatcher -> Function handler 
+
 app.engine('.hbs', handlebars({extname: '.hbs'}))
 app.set('view engine', '.hbs');
 app.set('views', path.join(__dirname, 'resource/views'));
 
-app.get('/', (req, res) => {
-  res.render('home');
-})
+// Home, search, contact
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
-})
+});
